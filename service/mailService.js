1dda36d491d55e7ea95065d2e3725fdb3cfc9278
OvikGukeyan <105244612+OvikGukeyan@users.noreply.github.com>
2024-03-17 19:51:13 +0100

    nodemailer

--- service/mailService.js
+++ service/mailService.js
@@ -1,7 +1,35 @@
-class MailService {
-    async sendActivationMail (to, link) {
+import nodemailer from 'nodemailer';
 
+class MailService {
+    constructor() {
+        this.transporter = nodemailer.createTransport({
+            host: 'smtp.ukr.net',
+            port: 465,
+            secure: false,
+            auth: {
+                user: 'ovikhukieian@ukr.net',
+                pass: 'ByLlt580YYfmSYei'
+            }
+        })
+    }
+    async sendActivationMail(to, link) {
+        await this.transporter.sendMail({
+            from: 'ovikhukieian@ukr.net',
+            to,
+            subject: 'Email activation',
+            text: '',
+            html:
+                `
+                    <div>
+                        <h1>To activate follow the link</h1>
+                        <a href="${link}"> ${link} </a>
+                    </div>
+                `
+
+        }, (err) => {
+            console.log(err)
+        })
     }
 }
 
-export default new MailService;
+export default new MailService();
