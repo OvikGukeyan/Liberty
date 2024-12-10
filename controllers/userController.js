8e79d9614cef8f450bd12c1df1adb47542976ef9
Ovik Gukeyan <0953188061ovik@gmail.com>
2024-12-10 23:18:45 +0100

    set-cookies-secure

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,7 +3,9 @@ import userService from "../service/userService.js";
 import { validationResult } from "express-validator";
 
 class UserController {
+    
     async registration(req, res, next) {
+        const isProduction = process.env.NODE_ENV === 'production';
         try {
             const errors = validationResult(req);
             if(!errors.isEmpty()) {
@@ -11,7 +13,7 @@ class UserController {
             }
             console.log(req.body)
             const userData = await userService.registration(req.body);
-            res.cookie('refreshToken', userData.refreshToken, {maxAge: 30 * 24 * 60 * 60 * 1000, httpOnly: true})
+            res.cookie('refreshToken', userData.refreshToken, {maxAge: 30 * 24 * 60 * 60 * 1000, httpOnly: true, secure: isProduction, sameSite: isProduction ? 'None' : 'Lax',})
             return res.json(userData)
         } catch (error) {
             next(error)
@@ -19,10 +21,11 @@ class UserController {
     }
 
     async login(req, res, next) {
+        const isProduction = process.env.NODE_ENV === 'production';
         try {
             const {email, password} = req.body;
             const userData = await userService.login(email, password);
-            res.cookie('refreshToken', userData.refreshToken, {maxAge: 30 * 24 * 60 * 60 * 1000, httpOnly: true})
+            res.cookie('refreshToken', userData.refreshToken, {maxAge: 30 * 24 * 60 * 60 * 1000, httpOnly: true, secure: isProduction, sameSite: isProduction ? 'None' : 'Lax',})
             return res.json(userData)
         } catch (error) {
             next(error)
@@ -51,10 +54,11 @@ class UserController {
     }
 
     async refresh(req, res, next) {
+        const isProduction = process.env.NODE_ENV === 'production';
         try {
             const {refreshToken} = req.cookies;
             const userData = await userService.refresh(refreshToken);
-            res.cookie('refreshToken', userData.refreshToken, {maxAge: 30 * 24 * 60 * 60 * 1000, httpOnly: true})
+            res.cookie('refreshToken', userData.refreshToken, {maxAge: 30 * 24 * 60 * 60 * 1000, httpOnly: true, secure: isProduction, sameSite: isProduction ? 'None' : 'Lax',})
             return res.json(userData)
         } catch (error) {
             next(error) 
